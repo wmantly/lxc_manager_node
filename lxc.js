8208f61f5a81955b841310dbabead2dff9180f4b
william <wmantly@gmail.com>
2016-01-29 17:14:10 -0500

    stop

--- lxc.js
+++ lxc.js
@@ -3,7 +3,7 @@ module.exports = function(config){
     var cmd = require('node-cmd');
 
     var sysExec = function(command, callback){
-        console.log('sysExec: ', command, '||| callback:' callback)
+        console.log('sysExec: ', command, '||| callback:', callback)
         cmd.get('unset XDG_SESSION_ID XDG_RUNTIME_DIR; cgm movepid all virt $$; '+command, callback)
     }
 /*    var obj = {};
