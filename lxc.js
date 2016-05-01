651a4d82a596f4431f10adedb98ff8fd75e10387
william <wmantly@gmail.com>
2016-04-30 23:28:59 -0400

    ssh

--- lxc.js
+++ lxc.js
@@ -2,7 +2,8 @@
 var exec = require('child_process').exec;
 
 function sysExec(command, callback){
-	command = 'ssh virt@104.236.77.157 ' + command;
+	command = new Buffer(command).toString('base64')
+	command = 'ssh virt@104.236.77.157 "' + command + '|base64 --decode|bash"';
 	// command = 'unset XDG_SESSION_ID XDG_RUNTIME_DIR; cgm movepid all virt $$; ' + command;
 
 	return exec(command, (function(callback){
