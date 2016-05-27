7d4965b21e722d4c7f5165380f3b2c79983b91b2
william <wmantly@gmail.com>
2016-05-27 00:34:24 -0400

    stuff

--- routes/api.js
+++ routes/api.js
@@ -158,7 +158,7 @@ var getAvailrunner = function(runner){
 
 var startRunners = function(worker, stopPercent){
 	console.log('starting runners on', worker.name)
-	stopPercent = stopPercent || 80;
+	stopPercent = stopPercent || 15;
 	ramPercentUsed(worker.ip, function(usedMemPercent){
 		if(usedMemPercent < stopPercent ){
 			var name = 'crunner-'+(Math.random()*100).toString().replace('.','');
