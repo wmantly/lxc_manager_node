00bca4ff34f6f5e5b717a6c90779fec9581b8c3c
william <wmantly@gmail.com>
2016-05-30 23:21:28 -0400

    stuff

--- routes/api.js
+++ routes/api.js
@@ -196,7 +196,7 @@ var run = function(req, res, runner, count){
 	};
 	console.log('run', runner);
 
-	if(!runner || count > 3){
+	if(!runner){
 		console.log('no runner');
 		res.status(503);
 		return res.json({error: 'No runners, try again soon.'});
